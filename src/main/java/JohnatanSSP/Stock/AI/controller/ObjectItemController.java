8ff184782017f9c5484be949284d339f17f74a82
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-09-12 01:13:52 -0300

    feat: adjustments controller for Mapper and DTO

--- src/main/java/JohnatanSSP/Stock/AI/controller/ObjectItemController.java
+++ src/main/java/JohnatanSSP/Stock/AI/controller/ObjectItemController.java
@@ -1,35 +1,62 @@
 package JohnatanSSP.Stock.AI.controller;
 
+import JohnatanSSP.Stock.AI.DTO.ObjectDTO;
 import JohnatanSSP.Stock.AI.model.ObjectItem;
 import JohnatanSSP.Stock.AI.service.ObjectItemService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stock")
 public class ObjectItemController {
 
-    private ObjectItemService Service;
+    private final ObjectItemService service;
 
     public ObjectItemController(ObjectItemService service) {
-        Service = service;
+        this.service = service;
     }
 
-    //get
-    public ShowAll(){
-
+    //get ALL
+    @GetMapping
+    public ResponseEntity<List<ObjectDTO>> ShowAll(){
+        List<ObjectDTO> objects = service.showAll();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(objects);
     }
-    //post
-    public ResponseEntity<ObjectItem> saveObject(@RequestBody ObjectItem objectItem){
-        ObjectItem object = Service.saveObject(objectItem);
-        return ResponseEntity.ok(object);
+
+    //get by ID
+    @GetMapping("/id")
+    public ResponseEntity<?> showById(@PathVariable Long id){
+        ObjectDTO object = service.showById(id);
+        if (object == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("not found");
+        }else{
+            return ResponseEntity.ok().body("Found succes");
+        }
+    }
+    //create
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody ObjectDTO object){
+
+        ObjectDTO newObject = service.saveObject(object);
+        return ResponseEntity.ok("saved");
     }
 
     //update
 
     //delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        if (service.showById(id)!= null) {
+            service.delete(id);
+            return ResponseEntity.ok("Object deleted"); // 204
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
 
