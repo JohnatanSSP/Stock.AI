d41876f4ce05832474710c3e9718e8bacd25f158
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-09-12 01:12:54 -0300

    feat: building DTO

---
+++ src/main/java/JohnatanSSP/Stock/AI/DTO/ObjectDTO.java
@@ -0,0 +1,24 @@
+package JohnatanSSP.Stock.AI.DTO;
+
+import JohnatanSSP.Stock.AI.enums.category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ObjectDTO {
+    // DTO para criar/atualizar
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private String image;
+    private category category;
+    private LocalDateTime validity;
+
+}
