a6590e50a1f4f35b24e4c9ce4e88b91fae5ed4fc
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-09-12 01:12:40 -0300

    feat: building mapper

---
+++ src/main/java/JohnatanSSP/Stock/AI/mapper/ObjectMapper.java
@@ -0,0 +1,40 @@
+package JohnatanSSP.Stock.AI.mapper;
+
+import JohnatanSSP.Stock.AI.DTO.ObjectDTO;
+import JohnatanSSP.Stock.AI.model.ObjectItem;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectMapper {
+
+    // DTO -> Entity
+    public static ObjectItem map(ObjectDTO dto) {
+        if (dto == null) return null;
+        ObjectItem obj = new ObjectItem();
+        obj.setId(dto.getId());
+        obj.setName(dto.getName());
+        obj.setPrice(dto.getPrice());
+        obj.setCategory(dto.getCategory());
+        obj.setQuantity(dto.getQuantity());
+        obj.setValidity(dto.getValidity());
+        obj.setImage(dto.getImage());
+        obj.setDescription(dto.getDescription());
+        return obj;
+    }
+
+    // Entity -> DTO
+    public static ObjectDTO map(ObjectItem obj) {
+
+        ObjectDTO objectDTO = new ObjectDTO();
+        objectDTO.setId(obj.getId());
+        objectDTO.setName(obj.getName());
+        objectDTO.setPrice(obj.getPrice());
+        objectDTO.setCategory(obj.getCategory());
+        objectDTO.setQuantity(obj.getQuantity());
+        objectDTO.setValidity(obj.getValidity());
+        objectDTO.setImage(obj.getImage());
+        objectDTO.setDescription(obj.getDescription());
+
+        return objectDTO;
+    }
+    }
